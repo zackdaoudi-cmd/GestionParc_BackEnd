adc0390c96a8b39f215f805d31b23ce6b23dc26e
daoudi zakaria <mr.zakariadaoudi@gmail.com>
2021-03-08 11:29:36 +0100

    initial Commit

---
+++ src/main/java/com/GestionParc/Gestion_Parc/GestionParcApplication.java
@@ -0,0 +1,13 @@
+package com.GestionParc.Gestion_Parc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionParcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionParcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GestionParc/Gestion_Parc/GestionParcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GestionParc.Gestion_Parc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionParcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
